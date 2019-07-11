bb6371f4adec4b4311ecf64f8fa754cd9f1f5829
Amelia <brown.amelia@gmail.com>
2019-07-11 14:12:35 -0400

    added docstrings

--- fiji_utils/fiji_utils.py
+++ fiji_utils/fiji_utils.py
@@ -1,3 +1,13 @@
+"""
+utilities/convenience functions for fiji
+
+col -> column
+col_dict -> column dictionary, keys are column headings, values are columns -> dict {column_heading(str) : [column_data](list)}
+
+"""
+
+
+
 import os
 
 from ij import IJ, WindowManager
@@ -11,14 +21,15 @@ from ij.plugin.filter import Analyzer
 """ int to use to set measurements to all"""
 MEAS_ALL = 2092799
 
-## need to add close the other images
-def force_close_all() :
-	while WindowManager.getImageCount() > 0 :
-		imp = IJ.getImage()
-		imp.changes = False
-		imp.close()
+# ## need to add close the other images
+# def force_close_all() :
+# 	while WindowManager.getImageCount() > 0 :
+# 		imp = IJ.getImage()
+# 		imp.changes = False
+# 		imp.close()
 
 def force_close_all_images() :
+	"""close all open images without save dialog box"""
 	while WindowManager.getImageCount() > 0 :
 		imp = IJ.getImage()
 		imp.changes = False
@@ -26,10 +37,12 @@ def force_close_all_images() :
 
 
 def force_close(imp) :
+	"""close imp without save dialog box"""
 	imp.changes = False
 	imp.close()
 
 def add_roi(roi, name=None) :
+	"""add roi to RoiManager, if name is given, sets its name to name"""
 	rm = RoiManager.getRoiManager()
 	rm.addRoi(roi)
 	if name is not None :
@@ -39,35 +52,41 @@ def add_roi(roi, name=None) :
 ## obj should be a collection
 def jpprint(obj) :
 	"""
-		pprints java collections as well as python collections
-		obj should be a collection
+		pretty print for java collections as well as python collections
+		obj is assumed to be a collection
 	"""
 	for item in obj :
 		print(item)
 
 
 def getMeasurementInt() :
+	"""returns the int value of the measurements setting, made as a hack to translate a set of checkboxes to its corresponding number"""
 	a = Analyzer()
 	i = a.getMeasurements()
 	return i
 
 
 def setMeasurementInt(i) :
+	"""sets the measurements setting to the int value i, made as a hack to translate an number to its corresponding checkboxes"""
 	a = Analyzer()
 	a.setMeasurements(i)
 
 
 def swap_ground_colors() :
+	"""swap background and foreground color settings"""
 	cur_fore = Toolbar.getForegroundColor()
 	cur_back = Toolbar.getBackgroundColor()
 
 	Toolbar.setForegroundColor(cur_back)
 	Toolbar.setBackgroundColor(cur_fore)
 
-def roi_xy(roi) :
+def roi_box_xy(roi) :
+	"""returns (x,y) values of rois bounding box as tuple"""
+
 	return (roi.getXBase(), roi.getYBase())
 
 def roi_cent(roi, integer=False) :
+	"""returns (x,y) values of centroid as tuple"""
 	stats = roi.getStatistics()
 	if integer :
 		return (int(stats.xCentroid), int(stats.yCentroid))
@@ -75,45 +94,50 @@ def roi_cent(roi, integer=False) :
 		return (stats.xCentroid, stats.yCentroid)
 
 
-def rt_to_arr_dict() :
+def rt_to_col_dict() :
+	"""takes the ResultsTable and return its contents as an col_dict(dictionary whose keys are heads of columns and values are the columns)"""
 	rt = ResultsTable.getResultsTable()
 
 	headings = rt.getHeadings()
 
-	arr_dict = {}
+	col_dict = {}
 	for i in range(len(headings)) :
 		temp = rt.getColumn(i)
 		if temp is not None :
-			arr_dict[rt.getColumnHeading(i)] = list(temp)
+			col_dict[rt.getColumnHeading(i)] = list(temp)
 
-	return arr_dict
+	return col_dict
 
 
 def measure(imp, roi=None, headings=None) :
+	"""measures imp and returns all columns or only those in headings
+		warning.. changes measurement settings to all as a col_dict(dictionary whose keys are heads of columns and values are the columns)"""
+
 	setMeasurementInt(MEAS_ALL)
 
-	## possibly don't include roi
+	## possibly don't include roi at all - setRoi may overwite an existing roi
 	if roi is not None :
 		imp.setRoi(roi)
 
 	IJ.run(imp, "Measure", "");
 
-	arr_dict = rt_to_arr_dict()
+	col_dict = rt_to_col_dict()
 
 	if headings is not None :
-		return arr_dict
+		return col_dict
 	else :
-		out_arr_dict = {}
+		out_col_dict = {}
 		for heading in heading :
 			## LBYL
 			try :
-				out_arr_dict[heading] = arr_dict[heading]
+				out_col_dict[heading] = col_dict[heading]
 			except KeyError :
 				IJ.log('fiji_conv.measure() : no heading {}'.format(heading))
-		return out_arr_dict
+		return out_col_dict
 
 
 def dup_and_crop(imp, roi) :
+	"""returns a duplicate of imp cropped to roi"""
 	imp.setRoi(roi)
 	d = Duplicator()
 	cropped_imp = d.crop(imp)
@@ -121,6 +145,7 @@ def dup_and_crop(imp, roi) :
 
 
 def more_black_than_white(imp) :
+	"""returns True if the count of black pixels is greater than the count of white pixels, black->lowest_value, white->highest_value"""
 
 	ip = imp.getProcessor()
 	hist = ip.getHistogram()
@@ -131,6 +156,7 @@ def more_black_than_white(imp) :
 
 
 def roi_name_index_dict() :
+	"""create and return a dict that takes an image_name(str) and returns it's index(int)"""
 	rm = RoiManager.getRoiManager()
 
 	str_ind_dict = {}
@@ -140,6 +166,8 @@ def roi_name_index_dict() :
 
 
 def open_get_imp(imp_path) :
+	"""open or get image - uses filename for the image in WindowManager"""
+
 	imp_name = os.path.basename(imp_path)
 	imp = WindowManager.getImage(imp_name)
 
@@ -153,6 +181,7 @@ def open_get_imp(imp_path) :
 
 
 def open_get_roi(roi_path) :
+	"""open or get roi - uses filename for the roi in RoiManager"""
 	rm = RoiManager.getRoiManager()
 	roi_name_to_index = roi_name_index_dict()
 
